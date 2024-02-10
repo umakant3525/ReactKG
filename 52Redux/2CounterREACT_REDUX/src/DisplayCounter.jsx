042861c7bbb5478480e32410490c23ae74c8297c
umakant3525 <umakant3525@gmail.com>
2024-02-11 03:40:58 +0530

    Basic layout of redux counter app

---
+++ 52Redux/2CounterREACT_REDUX/src/DisplayCounter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DisplayCounter = () => {
+  return (
+    <div>
+      <p className="lead mb-4">
+        counter current value       </p>
+    </div>
+  );
+};
+
+export default DisplayCounter;
